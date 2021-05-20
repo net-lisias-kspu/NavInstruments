5573d9628246ef9788f5fa649a0d99752f5c2b7a
Lisias <me@lisias.net>
2021-05-20 07:48:55 -0300

    Adding ModuleManager Support.

---
+++ Source/NavUtilLib/ModuleManagerSupport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace KSP
+{
+	public static class ModuleManagerSupport
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			string[] r = {"NavInstruments"};
+			return r;
+		}
+	}
+}
