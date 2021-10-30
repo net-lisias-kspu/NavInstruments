47be628eb3cf17a83e7db777b94b87b1e9e1d831
Lisias T <me@lisias.net>
2021-10-30 09:21:45 -0300

    Missing miscelannea details

--- Source/NavUtilLib/Properties/LegalMamboJambo.cs
+++ Source/NavUtilLib/Properties/LegalMamboJambo.cs
@@ -3,7 +3,7 @@ namespace NavInstruments
 {
 	public static class LegalMamboJambo
 	{
-		public const string Company = "L Aerospace KSP Division";
+		public const string Company = "/L Aerospace KSP Division";
 		public const string Copyight = "© 2020-2021 LisiasT";
 		public const string Product = "NavInstruments";
 		public const string Trademark = "NavInstruments™ by Kujuman, Continued by Ser, /L Unleashed™ by LisiasT";
